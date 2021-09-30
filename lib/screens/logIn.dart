f21da8ad335df596a968fd79061186275e116d66
irfancp <cpirfan7@gmail.com>
2021-09-30 10:25:23 +0530

    ...

--- lib/screens/logIn.dart
+++ lib/screens/logIn.dart
@@ -15,6 +15,7 @@ class LogIn extends StatefulWidget {
 class _LogInState extends State<LogIn> {
   TextEditingController emailController = TextEditingController();
   TextEditingController password = TextEditingController();
+    final _formKey = GlobalKey<FormState>();
   final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
@@ -30,95 +31,111 @@ class _LogInState extends State<LogIn> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          children: [
-            Container(
-                margin:
-                    EdgeInsets.only(top: 50, left: 100, right: 100, bottom: 50),
-                child: Image.asset("images/appLogo.png")),
-            Container(
-              height: 80,
-              margin: EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: TextFormField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    contentPadding: EdgeInsets.only(left: 5),
-                    border: InputBorder.none,
-                    labelText: "Username",
-                    labelStyle: TextStyle(color: Colors.black)),
-              ),
-            ),
-            Container(
-              height: 80,
-              margin: EdgeInsets.all(30),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: TextFormField(
-                controller: password,
-                decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    contentPadding: EdgeInsets.only(left: 5),
-                    border: InputBorder.none,
-                    labelText: "Password",
-                    labelStyle: TextStyle(color: Colors.black)),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(20),
-              child: ElevatedButton(
-                onPressed: ()async {
-                  print(emailController.text);
-                  print(password.text);
-                  try {
-                    await FirebaseAuth.instance
-                        .signInWithEmailAndPassword(
-                            email: emailController.text,
-                            password: password.text)
-                        .then((value) => Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(builder: (context) => Home())));
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'user-not-found') {
-                      Fluttertoast.showToast(
-                          msg: "No user found for that email",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.black,
-                          textColor: Colors.white,
-                          fontSize: 16.0);
-                      print('No user found for that email.');
-                    } else if (e.code == 'wrong-password') {
-                      Fluttertoast.showToast(
-                          msg: "Wrong password provided for that user.",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.black,
-                          textColor: Colors.white,
-                          fontSize: 16.0);
-                      print('Wrong password provided for that user.');
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Container(
+                  margin:
+                      EdgeInsets.only(top: 50, left: 100, right: 100, bottom: 50),
+                  child: Image.asset("images/appLogo.png")),
+              Container(
+                height: 80,
+                margin: EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter email';
                     }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-                child: Text("Login"),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black54,
-                  shape: CircleBorder(side: BorderSide(color: Colors.white)),
-                  padding: EdgeInsets.all(30),
+                    return null;
+                  },
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.only(left: 5),
+                      border: InputBorder.none,
+                      labelText: "Username",
+                      labelStyle: TextStyle(color: Colors.black)),
                 ),
               ),
-            )
-          ],
+              Container(
+                height: 80,
+                margin: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter password';
+                    }
+                    return null;
+                  },
+                  controller: password,
+                  decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.only(left: 5),
+                      border: InputBorder.none,
+                      labelText: "Password",
+                      labelStyle: TextStyle(color: Colors.black)),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      try {
+                        await FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                                email: emailController.text,
+                                password: password.text)
+                            .then((value) => Navigator.of(context)
+                                .pushReplacement(MaterialPageRoute(
+                                    builder: (context) => Home())));
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'user-not-found') {
+                          Fluttertoast.showToast(
+                              msg: "No user found for that email",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.black,
+                              textColor: Colors.white,
+                              fontSize: 16.0);
+                          print('No user found for that email.');
+                        } else if (e.code == 'wrong-password') {
+                          Fluttertoast.showToast(
+                              msg: "Wrong password provided for that user.",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.black,
+                              textColor: Colors.white,
+                              fontSize: 16.0);
+                          print('Wrong password provided for that user.');
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                    }
+                  },
+                  child: Text("Login"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black54,
+                    shape: CircleBorder(side: BorderSide(color: Colors.white)),
+                    padding: EdgeInsets.all(30),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     ));
